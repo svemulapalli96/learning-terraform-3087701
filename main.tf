dc928e3e17066f75ab895f330f05f041eb1ed116
svemulapalli96 <svemulapalli96@gmail.com>
2023-02-09 15:26:41 +0530

    Corrected alb vpc_id

--- main.tf
+++ main.tf
@@ -50,7 +50,7 @@ module "alb" {
 
   load_balancer_type = "application"
 
-  vpc_id             = module.blog_sg.vpc_id
+  vpc_id             = module.blog_vpc.vpc_id
   subnets            = module.vpc.public_subnets
   security_groups    = module.blog_sg.security_group_id
 
